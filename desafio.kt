87b37f177c41044d76fde3e9310a475d203dd9f7
michelypsouza <michely.psouza@gmail.com>
2024-02-27 00:00:05 -0300

    adicionando uso para o enum existente

--- desafio.kt
+++ desafio.kt
@@ -1,10 +1,8 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario(val nome: String, val matricula: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -21,17 +19,20 @@ fun main() {
     val usuario1 = Usuario("Michely", 1)
     val usuario2 = Usuario("Paulo", 2)
 
-    val conteudo1 = ConteudoEducacional("Linguagem Kotlin")
-    val conteudo2 = ConteudoEducacional("Padrões de Projeto com Kotlin")
-    val conteudo3 = ConteudoEducacional("Spring Boot com Kotlin")
+    val conteudo1 = ConteudoEducacional("Lógica de Programação", 360)
+    val conteudo2 = ConteudoEducacional("Padrões de Projeto com Kotlin", nivel=Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional(nome = "Spring Boot com Kotlin", duracao = 300, nivel=Nivel.DIFICIL)
 
     val formacaoKotlin = Formacao("Formação Backend com Kotlin", listOf(conteudo1, conteudo2, conteudo3))
     formacaoKotlin.matricular(usuario1)
     formacaoKotlin.matricular(usuario2)
 
-    println("A formação ${formacaoKotlin.nome} possui os seguintes usuários matriculados : ${formacaoKotlin.inscritos.map { it.nome }}")
+    println("A formação ${formacaoKotlin.nome} possui os seguintes usuários matriculados : ${formacaoKotlin.inscritos.map { it.nome }}\n")       
+
+    val formacaoLogicaProg = Formacao("Formação Básica de Lógica de Programação", listOf(conteudo1))
+    formacaoLogicaProg.matricular(usuario1)
+    println("A formação ${formacaoLogicaProg.nome} possui os seguintes usuários matriculados : ${formacaoLogicaProg.inscritos.map { it.nome }}")  
 
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
-
